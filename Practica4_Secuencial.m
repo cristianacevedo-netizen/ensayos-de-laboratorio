a3dc8cce68f3bbaaf2d6efe48152ddd31444c539
cristianacevedo-netizen <cristianacevedo@gmail.com>
2026-04-16 00:03:33 -0400

    Updated the order of archivos and etiquetas arrays in Practica4_Secuencial.m

---
+++ Practica4_Secuencial.m
@@ -0,0 +1,9 @@
+archivos = {
+    'Test-BS1Calitri35XY_sin_galgas.dat',
+    'Test-BS1Calitri50XY_sin_galgas.dat',
+    'Test-BS1Calitri100XY_sin_galgas.dat',
+    'Test-BS1Calitri200XY_sin_galgas.dat',
+    'Test-BS1Calitri200bisXY_sin_galgas.dat',
+    'Test-BS1Calitri300XY_sin_galgas.dat'
+};
+etiquetas = {'Calitri35','Calitri50','Calitri100','Calitri200','Calitri200bis','Calitri300'};
